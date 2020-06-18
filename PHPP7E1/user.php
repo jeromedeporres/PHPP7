28e73549594be48c7733e6c9fef73b1dff3206a0
Jérôme DE PORRES <62543727+jeromedeporres@users.noreply.github.com>
2020-06-18 11:14:20 +0200

    Update user.php

--- PHPP7E1/user.php
+++ PHPP7E1/user.php
@@ -1,11 +1,34 @@
+<?php
+    $regexName = '^[\p{L}]{1}[\' \-\p{L}]+$^';
+    if(!empty($_GET['lastname'])) {
+        if(preg_match($regexName, $_GET['lastname'])) {
+            $lastname = 'Votre nom de famille est ' . $_GET['lastname'];
+        }else {
+            $lastnameError = 'Veuillez renseigner un nom de famille valide';
+        }
+    }else {
+        $lastnameError = 'Veuillez renseigner votre nom de famille';
+    }
+
+    if(!empty($_GET['firstname'])) {
+        if(preg_match($regexName, $_GET['firstname'])) {
+            $firstname = 'Votre prénom est ' . $_GET['firstname'];
+        }else {
+            $firstnameError = 'Veuillez renseigner un prénom valide';
+        }
+    }else {
+        $firstnameError = 'Veuillez renseigner votre prénom';
+    }
+?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" dir="ltr">
 <head>
-    <meta charset="UTF-8" dir="ltr" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultat P7E1</title>
+    <meta charset="UTF-8" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vos informations</title>
 </head>
 <body>
-    
+    <p><?= isset($lastname) ? $lastname : $lastnameError ?></p>
+    <p><?= isset($firstname) ? $firstname : $firstnameError ?></p>
 </body>
 </html>
